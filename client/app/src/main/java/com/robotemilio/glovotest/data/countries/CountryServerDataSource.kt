43e0dccb79023444a5ca270992db8b1a748a478a
Antonio Suarez Galan <antonio.suarezgalan@soprasteria.com>
2019-03-01 19:26:36 +0100

    Drawing polygons and network error implemented

--- client/app/src/main/java/com/robotemilio/glovotest/data/countries/CountryServerDataSource.kt
+++ client/app/src/main/java/com/robotemilio/glovotest/data/countries/CountryServerDataSource.kt
@@ -2,6 +2,7 @@ package com.robotemilio.glovotest.data.countries
 
 import com.robotemilio.glovotest.data.CountriesApi
 import com.robotemilio.glovotest.data.common.ServerDataSource
+import com.robotemilio.glovotest.domain.exception.CustomException
 import io.reactivex.Flowable
 import javax.inject.Inject
 
@@ -10,6 +11,9 @@ class CountryServerDataSource @Inject constructor(private val countriesApi: Coun
 
     override fun list(): Flowable<List<CountryDTO>> {
         return countriesApi.getCountryList()
+            .onErrorResumeNext { t: Throwable ->
+                Flowable.error(CustomException(CustomException.Layer.DATA_SOURCE, CustomException.Code.NETWORK_ERROR))
+            }
     }
 
 }
--- client/app/src/main/java/com/robotemilio/glovotest/ui/map/MapsActivity.kt
+++ client/app/src/main/java/com/robotemilio/glovotest/ui/map/MapsActivity.kt
@@ -1,18 +1,27 @@
 package com.robotemilio.glovotest.ui.map
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import com.robotemilio.glovotest.R
+import com.robotemilio.glovotest.domain.exception.CustomException
+import com.robotemilio.glovotest.domain.exception.CustomException.Code.NETWORK_ERROR
+import com.robotemilio.glovotest.domain.exception.CustomException.Code.UNKNOWN
 import com.robotemilio.glovotest.domain.model.City
 import com.robotemilio.glovotest.domain.model.Country
 import com.robotemilio.glovotest.extensions.getViewModel
+import com.robotemilio.glovotest.extensions.logError
 import com.robotemilio.glovotest.extensions.observe
 import com.robotemilio.glovotest.ui.common.BaseActivity
 
@@ -20,7 +29,7 @@ class MapsActivity : BaseActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
-    private lateinit var mapViewModel : MapsActivityViewModel
+    private lateinit var mapViewModel: MapsActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +52,7 @@ class MapsActivity : BaseActivity(), OnMapReadyCallback {
         observe(mapViewModel.countriesLiveData, ::onCountriesReceived)
         observe(mapViewModel.citiesLiveData, ::onCitiesReceived)
         observe(mapViewModel.currentCityLiveData, ::onCurrentCityChanged)
+        observe(mapViewModel.errorsReceived, ::handleErrors)
 
         mapViewModel.getCountryList()
         mapViewModel.getCityList()
@@ -54,8 +64,23 @@ class MapsActivity : BaseActivity(), OnMapReadyCallback {
 
     private fun onCitiesReceived(cities: List<City>?) {
         Log.d("ANTONIO", cities.toString())
-        cities?.first()?.also {
-            mapViewModel.getCityInfo(it.code)
+
+        cities?.forEach(::drawPolygon)
+
+        mapViewModel.getCityInfo("BCN")
+    }
+
+    fun drawPolygon(city: City) {
+        city.workingArea.forEach {
+            if (it.points.size > 2) {
+                mMap.addPolygon(
+                    PolygonOptions()
+                        .fillColor(ContextCompat.getColor(this, R.color.transparentRed))
+                        .strokeColor(ContextCompat.getColor(this, R.color.lessTransparentRed))
+                        .addAll(it.points)
+                        .add(it.points[0])
+                )
+            }
         }
     }
 
@@ -80,4 +105,25 @@ class MapsActivity : BaseActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    private fun handleErrors(throwable: Throwable?) {
+        throwable?.let {
+            when (it) {
+                is CustomException -> handleCustomException(it)
+                else -> logError(it, true)
+            }
+        }
+    }
+
+    private fun handleCustomException(customException: CustomException) {
+        when (customException.code) {
+            NETWORK_ERROR -> {
+                Toast.makeText(this, "Network error", Toast.LENGTH_SHORT).show()
+            }
+            UNKNOWN -> {
+                logError(customException, true)
+            }
+        }
+    }
+
 }
